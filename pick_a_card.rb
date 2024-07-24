7fe8b52ff6492ab8541d8491f5c919953fdf4a38
Марина Дегтярёва <degtyareva1930@mail.ru>
2024-07-24 23:11:32 +0500

    vers2

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,7 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] 
 
-puts "#{values.sample} of #{suits.sample}"
+#масти из таблицы ASCII
+suits = [3.chr, 4.chr, 5.chr, 6.chr]    
+
+#вывод рандомной карты
+puts "#{values.sample}#{suits.sample}"
